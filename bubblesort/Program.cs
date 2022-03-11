e06b67702c985b11000a31cb2b62217be543cdd3
Haitham Kazzaz <haitham.k2@gso.schule.koeln>
2022-03-11 12:56:10 +0100

    bubblesort angefangen

---
+++ bubblesort/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace bubblesort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag;
+            int n = 4;
+            int[] Daten ={45, 10, 15, 4};
+
+            foreach(int element in Daten)
+            {
+                Console.Write(" "+element);
+            }
+            Console.WriteLine();
+
+            do
+            {
+                flag = true;
+                for (int index = 0; index < n - 1; index++)
+                {
+
+
+                    if (Daten[index] > Daten[index + 1])
+                    {
+                        int buffer = Daten[index + 1];
+                        //{45, 10, 15, 4}
+                        Daten[index + 1] = Daten[index];
+                        //{45, 45, 15, 4}
+                        Daten[index] = buffer;
+                    }
+                }
+            } while(flag);
+
+            
+            
+            //
+
+            foreach (int element in Daten)
+            {
+                Console.Write(" " + element);
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+
+        }
+    }
+}
