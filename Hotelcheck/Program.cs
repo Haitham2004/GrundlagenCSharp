e2d6d037a631098e52e4d636c806cb60a030ff7c
Haitham Kazzaz <haitham.k2@gso.schule.koeln>
2022-04-08 12:51:19 +0200

    führt nicht weiter

--- Hotelcheck/Program.cs
+++ Hotelcheck/Program.cs
@@ -7,8 +7,8 @@ namespace Hotelcheck
         static void Main(string[] args)
         {
             short[] zimmernummer = { 23, 32, 45, 3 };
-            string[] vorname = { "Paul, Ringo, John, George" };
-            string[] nachname = { "McCartney, Starr, Lennon, Harrison" };
+            string[] vorname = { "Paul", "Ringo", "John", "George" };
+            string[] nachname = { "McCartney", "Starr", "Lennon", "Harrison" };
             short[] übernachtungen = { 4, 3, 1, 12 };
             double[] rechnungsbetrag = { 329.84, 247.38, 82.46, 989.52 };
             bool kundencheck = false;
@@ -19,14 +19,19 @@ namespace Hotelcheck
                 Console.WriteLine("------------------------------------------------------------------------------------\n" +
                                  "                              >>> Hotel-Check In <<<\n" +
                                  "------------------------------------------------------------------------------------\n\n");
-                Console.WriteLine("Herzlich Willkommen​");
+                Console.WriteLine("Herzlich Willkommen");
                 Console.WriteLine("Bitte geben Sie die Zimmernummer oder Nachname");
+                Console.WriteLine("Exit tippen für out-Check");
                 string eingabe_string = Console.ReadLine();
                 short eingabe_short;
-
+                
+                Console.WriteLine("------------------------------------------------------------------------------------\n" +
+                                "                              >>> Hotel-Check out <<<\n" +
+                                "------------------------------------------------------------------------------------\n\n");
 
 
                 flag = short.TryParse(eingabe_string, out eingabe_short);
+
 
 
 
@@ -35,11 +40,13 @@ namespace Hotelcheck
                     var daten = GetData(eingabe_short, zimmernummer, vorname, nachname, übernachtungen, rechnungsbetrag);
 
 
-
-                    if (daten.Item1 == true)
+                    if(daten.Item1 == true)
                     {
-                        
-
+                        short zimmernummer_gast  = daten.Item2;
+                        string vorname_gast = daten.Item3;
+                        string nachname_gast = daten.Item4;
+                        short übernachtungen_gast = daten.Item5;
+                        double rechnungsbetrag_gast = daten.Item6;
                     }
                     else
                     {
@@ -51,11 +58,17 @@ namespace Hotelcheck
                 }
                 else
                 {
+
+                    
                     var daten = GetData(eingabe_string, zimmernummer, vorname, nachname, übernachtungen, rechnungsbetrag);
                     if (daten.Item1 == true)
                     {
 
-
+                        short zimmernummer_gast = daten.Item2;
+                        string vorname_gast = daten.Item3;
+                        string nachname_gast = daten.Item4;
+                        short übernachtungen_gast = daten.Item5;
+                        double rechnungsbetrag_gast = daten.Item6;
 
                     }
                     else
@@ -70,9 +83,12 @@ namespace Hotelcheck
 
                 }
 
+                if (eingabe_string == "exit")
+                {
+                    Environment.Exit(0);
+                }
 
-
-            } while (flag == true);
+            } while (true);
 
 
 
